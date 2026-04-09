9adeb0574b037156285b110958256611ab137461
Noam-Journo <noamjorno6@gmail.com>
2026-04-09 14:10:58 +0300

    update workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,7 @@ module "ecr" {
 }
 module "app_secrets" {
   source      = "./modules/secrets"
-  secret_name = "${var.cluster_name}-db-pass"
+  secret_name = "${var.cluster_name}-db-pass-v2"
   tags        = local.common_tags
 }
 module "kubernetes" {
